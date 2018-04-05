8e9b6890fb4a2288ed38393ba5aabecec1669800
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-04-05 14:09:50 -0400

    Add files via upload

---
+++ Truck.java
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *
+ * Created by: Heejo Suh
+ * Created on: Apr 2018
+ * Created for: learning
+ * 
+ * This program is a sub-blueprint of 'Vehicle'
+ ****************************************************************************/
+
+//Jet is the sub-class of Vehicle
+public class Truck extends Vehicle {
+	//class for truck
+	
+	public static final double MULTIPLIER = 0.4; 
+	
+	public Truck(String plate, String colour) throws Exception {
+		super(plate, colour); 
+		super.setNumberOfDoors(2);
+		
+	}
+
+	public void Accelerate(double speed) {
+		super.Accelerate(speed*MULTIPLIER);
+	}
+	
+	public void Decelerate(double speed) {
+		super.Decelerate(speed*MULTIPLIER); 
+	}
+}
